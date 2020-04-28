b0ef51d44c1e217368b3bfb8ec0affb5b166f8ec
Ciprian Jichici <ciprian.jichici@genisoft.eu>
2020-04-28 07:46:50 +0300

    Automation

--- artifacts/environment-setup/solliance-synapse-automation/solliance-synapse-automation.psm1
+++ artifacts/environment-setup/solliance-synapse-automation/solliance-synapse-automation.psm1
@@ -1,3 +1,34 @@
+function List-StorageAccountKeys {
+
+    param(
+    [parameter(Mandatory=$true)]
+    [String]
+    $SubscriptionId,
+
+    [parameter(Mandatory=$true)]
+    [String]
+    $ResourceGroupName,
+
+    [parameter(Mandatory=$true)]
+    [String]
+    $Name,
+
+    [parameter(Mandatory=$true)]
+    [String]
+    $Token
+    )
+
+    $uri = "https://management.azure.com/subscriptions/$($SubscriptionId)/resourcegroups/$($ResourceGroupName)/providers/Microsoft.Storage/storageAccounts/$($Name)/listKeys?api-version=2015-05-01-preview"
+
+    Write-Output "Calling endpoint $uri"
+
+    $result = Invoke-RestMethod  -Uri $uri -Method POST -Headers @{ Authorization="Bearer $Token" } -ContentType "application/json"
+ 
+    Write-Output $result
+
+    return $result.key1
+}
+
 function Create-KeyVaultLinkedService {
     
     param(
@@ -18,13 +49,44 @@ function Create-KeyVaultLinkedService {
     $Token 
     )
 
-    $keyVaultTemplate = Get-Content -Path "$($TemplatesPath)/key_vault.json"
+    $keyVaultTemplate = Get-Content -Path "$($TemplatesPath)/key_vault_linked_service.json"
     $keyVault = $keyVaultTemplate.Replace("#KEY_VAULT_NAME#", $Name)
     $uri = "https://$($WorkspaceName).dev.azuresynapse.net/linkedservices/$($Name)?api-version=2019-06-01-preview"
 
     Write-Output "Calling endpoint $uri"
 
-    $result = Invoke-RestMethod  -Uri $uri -Method PUT -Body $keyVault -Headers @{ Authorization="Bearer $Token" }
+    $result = Invoke-RestMethod  -Uri $uri -Method PUT -Body $keyVault -Headers @{ Authorization="Bearer $Token" } -ContentType "application/json"
+ 
+    Write-Output $result
+}
+
+function Create-BlobStorageLinkedService {
+    
+    param(
+    [parameter(Mandatory=$true)]
+    [String]
+    $TemplatesPath,
+
+    [parameter(Mandatory=$true)]
+    [String]
+    $WorkspaceName,
+
+    [parameter(Mandatory=$true)]
+    [String]
+    $Name,
+
+    [parameter(Mandatory=$true)]
+    [String]
+    $Token 
+    )
+
+    $keyVaultTemplate = Get-Content -Path "$($TemplatesPath)/blob_storage_linked_service.json"
+    $keyVault = $keyVaultTemplate.Replace("#STORAGE_ACCOUNT_NAME#", $Name)
+    $uri = "https://$($WorkspaceName).dev.azuresynapse.net/linkedservices/$($Name)?api-version=2019-06-01-preview"
+
+    Write-Output "Calling endpoint $uri"
+
+    $result = Invoke-RestMethod  -Uri $uri -Method PUT -Body $keyVault -Headers @{ Authorization="Bearer $Token" } -ContentType "application/json"
  
     Write-Output $result
 }
@@ -71,11 +133,12 @@ function Create-IntegrationRuntime {
 
     Write-Output "Calling endpoint $uri"
 
-    $result = Invoke-RestMethod  -Uri $uri -Method POST -Body $keyVault -Headers @{ Authorization="Bearer $Token" }
+    $result = Invoke-RestMethod  -Uri $uri -Method PUT -Body $integrationRuntime -Headers @{ Authorization="Bearer $Token" } -ContentType "application/json"
  
     Write-Output $result
 }
 
-
+Export-ModuleMember -Function List-StorageAccountKeys
 Export-ModuleMember -Function Create-KeyVaultLinkedService
+Export-ModuleMember -Function Create-BlobStorageLinkedService
 Export-ModuleMember -Function Create-IntegrationRuntime
