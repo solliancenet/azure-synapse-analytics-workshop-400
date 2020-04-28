cb11c8b54da0d0fb61b1db189a8c5cd22bc139a5
Ciprian Jichici <ciprian.jichici@genisoft.eu>
2020-04-28 04:31:46 +0300

    Setup environment

--- artifacts/environment-setup/solliance-synapse-automation/solliance-synapse-automation.psm1
+++ artifacts/environment-setup/solliance-synapse-automation/solliance-synapse-automation.psm1
@@ -1,4 +1,4 @@
-function Create-KeyVault-LinkedService {
+function Create-KeyVaultLinkedService {
     
     param(
     [parameter(Mandatory=$true)]
@@ -18,16 +18,64 @@ function Create-KeyVault-LinkedService {
     $Token 
     )
 
-    $keyVaultTemplate = Get-Content -Path "$($TemplatesPath)/keyvault.json"
+    $keyVaultTemplate = Get-Content -Path "$($TemplatesPath)/key_vault.json"
     $keyVault = $keyVaultTemplate.Replace("#KEY_VAULT_NAME#", $Name)
     $uri = "https://$($WorkspaceName).dev.azuresynapse.net/linkedservices/$($Name)?api-version=2019-06-01-preview"
 
-    Write-Output $keyVault
-    Write-Output $uri
+    Write-Output "Calling endpoint $uri"
 
     $result = Invoke-RestMethod  -Uri $uri -Method PUT -Body $keyVault -Headers @{ Authorization="Bearer $Token" }
  
     Write-Output $result
 }
 
-Export-ModuleMember -Function Create-KeyVault-LinkedService
+function Create-IntegrationRuntime {
+    
+    param(
+    [parameter(Mandatory=$true)]
+    [String]
+    $TemplatesPath,
+
+    [parameter(Mandatory=$true)]
+    [String]
+    $SubscriptionId,
+
+    [parameter(Mandatory=$true)]
+    [String]
+    $ResourceGroupName,
+
+    [parameter(Mandatory=$true)]
+    [String]
+    $WorkspaceName,
+
+    [parameter(Mandatory=$true)]
+    [String]
+    $Name,
+
+    [parameter(Mandatory=$true)]
+    [Int32]
+    $CoreCount,
+
+    [parameter(Mandatory=$true)]
+    [Int32]
+    $TimeToLive,
+
+    [parameter(Mandatory=$true)]
+    [String]
+    $Token
+    )
+
+    $integrationRuntimeTemplate = Get-Content -Path "$($TemplatesPath)/integration_runtime.json"
+    $integrationRuntime = $integrationRuntimeTemplate.Replace("#INTEGRATION_RUNTIME_NAME#", $Name).Replace("#CORE_COUNT#", $CoreCount).Replace("#TIME_TO_LIVE#", $TimeToLive)
+    $uri = "https://management.azure.com/subscriptions/$($SubscriptionId)/resourcegroups/$($ResourceGroupName)/providers/Microsoft.Synapse/workspaces/$($WorkspaceName)/integrationruntimes/$($Name)?api-version=2019-06-01-preview"
+
+    Write-Output "Calling endpoint $uri"
+
+    $result = Invoke-RestMethod  -Uri $uri -Method POST -Body $keyVault -Headers @{ Authorization="Bearer $Token" }
+ 
+    Write-Output $result
+}
+
+
+Export-ModuleMember -Function Create-KeyVaultLinkedService
+Export-ModuleMember -Function Create-IntegrationRuntime
