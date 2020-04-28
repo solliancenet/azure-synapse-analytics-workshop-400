10735465d1fadec5da0bec05c78e22e8b7473872
Ciprian Jichici <ciprian.jichici@genisoft.eu>
2020-04-28 19:18:26 +0300

    Setup automation

--- artifacts/environment-setup/solliance-synapse-automation/solliance-synapse-automation.psm1
+++ artifacts/environment-setup/solliance-synapse-automation/solliance-synapse-automation.psm1
@@ -116,14 +116,45 @@ function Create-BlobStorageLinkedService {
     )
 
     $keyVaultTemplate = Get-Content -Path "$($TemplatesPath)/blob_storage_linked_service.json"
-    $keyVault = $keyVaultTemplate.Replace("#LINKED_SERVICE_NAME#", $Name).Replace("#STORAGE_ACCOUNT_NAME#", $Name).Replace("#STORAGE_ACCOUNT_KEY#", $Name)
+    $keyVault = $keyVaultTemplate.Replace("#LINKED_SERVICE_NAME#", $Name).Replace("#STORAGE_ACCOUNT_NAME#", $Name).Replace("#STORAGE_ACCOUNT_KEY#", $Key)
     $uri = "https://$($WorkspaceName).dev.azuresynapse.net/linkedservices/$($Name)?api-version=2019-06-01-preview"
-
-    Write-Debug "Calling endpoint $uri"
 
     $result = Invoke-RestMethod  -Uri $uri -Method PUT -Body $keyVault -Headers @{ Authorization="Bearer $Token" } -ContentType "application/json"
  
-    Write-Debug $result
+    return $result
+}
+
+function Create-DataLakeLinkedService {
+    
+    param(
+    [parameter(Mandatory=$true)]
+    [String]
+    $TemplatesPath,
+
+    [parameter(Mandatory=$true)]
+    [String]
+    $WorkspaceName,
+
+    [parameter(Mandatory=$true)]
+    [String]
+    $Name,
+
+    [parameter(Mandatory=$true)]
+    [String]
+    $Key,
+
+    [parameter(Mandatory=$true)]
+    [String]
+    $Token 
+    )
+
+    $itemTemplate = Get-Content -Path "$($TemplatesPath)/data_lake_linked_service.json"
+    $item = $itemTemplate.Replace("#LINKED_SERVICE_NAME#", $Name).Replace("#STORAGE_ACCOUNT_NAME#", $Name).Replace("#STORAGE_ACCOUNT_KEY#", $Key)
+    $uri = "https://$($WorkspaceName).dev.azuresynapse.net/linkedservices/$($Name)?api-version=2019-06-01-preview"
+
+    $result = Invoke-RestMethod  -Uri $uri -Method PUT -Body $item -Headers @{ Authorization="Bearer $Token" } -ContentType "application/json"
+    
+    return $result
 }
 
 function Create-CosmosDBLinkedService {
@@ -155,14 +186,61 @@ function Create-CosmosDBLinkedService {
     )
 
     $cosmosDbTemplate = Get-Content -Path "$($TemplatesPath)/cosmos_db_linked_service.json"
-    $cosmosDb = $cosmosDbTemplate.Replace("#LINKED_SERVICE_NAME#", $Name).Replace("#COSMOSDB_ACCOUNT_NAME#", $Name).Replace("#COSMOSDB_DDATABASE_NAME#", $Name).Replace("#COSMOSDB_ACCOUNT_KEY#", $Name)
-    $uri = "https://$($WorkspaceName).dev.azuresynapse.net/linkedservices/$($Name)?api-version=2019-06-01-preview"
+    $cosmosDb = $cosmosDbTemplate.Replace("#LINKED_SERVICE_NAME#", $Name).Replace("#COSMOSDB_ACCOUNT_NAME#", $Name).Replace("#COSMOSDB_DATABASE_NAME#", $Database).Replace("#COSMOSDB_ACCOUNT_KEY#", $Key)
+    $uri = "https://$($WorkspaceName).dev.azuresynapse.net/linkedServices/$($Name)?api-version=2019-06-01-preview"
 
-    Write-Debug "Calling endpoint $uri"
+    Write-Information "Calling endpoint $uri"
 
-    $result = Invoke-RestMethod  -Uri $uri -Method PUT -Body $keyVault -Headers @{ Authorization="Bearer $Token" } -ContentType "application/json"
+    $result = Invoke-RestMethod  -Uri $uri -Method PUT -Body $cosmosDb -Headers @{ Authorization="Bearer $Token" } -ContentType "application/json"
  
-    Write-Debug $result
+    Write-Information $result
+
+    return $result
+}
+
+function Create-SQLPoolKeyVaultLinkedService {
+    
+    param(
+    [parameter(Mandatory=$true)]
+    [String]
+    $TemplatesPath,
+
+    [parameter(Mandatory=$true)]
+    [String]
+    $WorkspaceName,
+
+    [parameter(Mandatory=$true)]
+    [String]
+    $Name,
+
+    [parameter(Mandatory=$true)]
+    [String]
+    $DatabaseName,
+
+    [parameter(Mandatory=$true)]
+    [String]
+    $UserName,
+
+    [parameter(Mandatory=$true)]
+    [String]
+    $KeyVaultLinkedServiceName,
+
+    [parameter(Mandatory=$true)]
+    [String]
+    $SecretName,
+
+    [parameter(Mandatory=$true)]
+    [String]
+    $Token 
+    )
+
+    $itemTemplate = Get-Content -Path "$($TemplatesPath)/sql_pool_key_vault_linked_service.json"
+    $item = $itemTemplate.Replace("#LINKED_SERVICE_NAME#", $Name).Replace("#WORKSPACE_NAME#", $WorkspaceName).Replace("#DATABASE_NAME#", $DatabaseName).Replace("#USER_NAME#", $UserName).Replace("#KEY_VAULT_LINKED_SERVICE_NAME#", $KeyVaultLinkedServiceName).Replace("#SECRET_NAME#", $SecretName)
+    $uri = "https://$($WorkspaceName).dev.azuresynapse.net/linkedServices/$($Name)?api-version=2019-06-01-preview"
+
+    $result = Invoke-RestMethod  -Uri $uri -Method PUT -Body $item -Headers @{ Authorization="Bearer $Token" } -ContentType "application/json"
+
+    return $result
 }
 
 function Create-IntegrationRuntime {
@@ -212,9 +290,212 @@ function Create-IntegrationRuntime {
     Write-Output $result
 }
 
+function Create-Dataset {
+    
+    param(
+    [parameter(Mandatory=$true)]
+    [String]
+    $DatasetsPath,
+
+    [parameter(Mandatory=$true)]
+    [String]
+    $WorkspaceName,
+
+    [parameter(Mandatory=$true)]
+    [String]
+    $Name,
+
+    [parameter(Mandatory=$true)]
+    [String]
+    $LinkedServiceName,
+
+    [parameter(Mandatory=$true)]
+    [String]
+    $Token 
+    )
+
+    $itemTemplate = Get-Content -Path "$($DatasetsPath)/$($Name).json"
+    $item = $itemTemplate.Replace("#LINKED_SERVICE_NAME#", $LinkedServiceName)
+    $uri = "https://$($WorkspaceName).dev.azuresynapse.net/datasets/$($Name)?api-version=2019-06-01-preview"
+
+    $result = Invoke-RestMethod  -Uri $uri -Method PUT -Body $item -Headers @{ Authorization="Bearer $Token" } -ContentType "application/json"
+    
+    return $result
+}
+
+function Create-Pipeline {
+    
+    param(
+    [parameter(Mandatory=$true)]
+    [String]
+    $PipelinesPath,
+
+    [parameter(Mandatory=$true)]
+    [String]
+    $WorkspaceName,
+
+    [parameter(Mandatory=$true)]
+    [String]
+    $Name,
+
+    [parameter(Mandatory=$true)]
+    [String]
+    $FileName,
+
+    [parameter(Mandatory=$true)]
+    [String]
+    $Token 
+    )
+
+    $itemTemplate = Get-Content -Path "$($PipelinesPath)/$($FileName).json"
+    $item = $itemTemplate
+    $uri = "https://$($WorkspaceName).dev.azuresynapse.net/pipelines/$($Name)?api-version=2019-06-01-preview"
+
+    $result = Invoke-RestMethod  -Uri $uri -Method PUT -Body $item -Headers @{ Authorization="Bearer $Token" } -ContentType "application/json"
+    
+    return $result
+}
+
+function Run-Pipeline {
+    
+    param(
+
+    [parameter(Mandatory=$true)]
+    [String]
+    $WorkspaceName,
+
+    [parameter(Mandatory=$true)]
+    [String]
+    $Name,
+
+    [parameter(Mandatory=$true)]
+    [String]
+    $Token 
+    )
+
+    $uri = "https://$($WorkspaceName).dev.azuresynapse.net/pipelines/$($Name)/createRun?api-version=2018-06-01"
+
+    $result = Invoke-RestMethod  -Uri $uri -Method POST -Headers @{ Authorization="Bearer $Token" } -ContentType "application/json"
+    
+    return $result
+}
+
+function Get-PipelineRun {
+    
+    param(
+
+    [parameter(Mandatory=$true)]
+    [String]
+    $WorkspaceName,
+
+    [parameter(Mandatory=$true)]
+    [String]
+    $RunId,
+
+    [parameter(Mandatory=$true)]
+    [String]
+    $Token 
+    )
+
+    $uri = "https://$($WorkspaceName).dev.azuresynapse.net/pipelineruns/$($RunId)?api-version=2019-06-01-preview"
+
+    $result = Invoke-RestMethod  -Uri $uri -Method GET -Headers @{ Authorization="Bearer $Token" }
+    
+    return $result
+}
+
+function Get-OperationResult {
+    
+    param(
+
+    [parameter(Mandatory=$true)]
+    [String]
+    $WorkspaceName,
+
+    [parameter(Mandatory=$true)]
+    [String]
+    $OperationId,
+
+    [parameter(Mandatory=$true)]
+    [String]
+    $Token 
+    )
+
+    $uri = "https://$($WorkspaceName).dev.azuresynapse.net/operationResults/$($OperationId)?api-version=2019-06-01-preview"
+
+    $result = Invoke-RestMethod  -Uri $uri -Method GET -Headers @{ Authorization="Bearer $Token" }
+    
+    return $result
+}
+
+function Wait-ForOperation {
+    
+    param(
+
+    [parameter(Mandatory=$true)]
+    [String]
+    $WorkspaceName,
+
+    [parameter(Mandatory=$true)]
+    [String]
+    $OperationId,
+
+    [parameter(Mandatory=$true)]
+    [String]
+    $Token 
+    )
+
+    $uri = "https://$($WorkspaceName).dev.azuresynapse.net/operationResults/$($OperationId)?api-version=2019-06-01-preview"
+    $result = Invoke-RestMethod  -Uri $uri -Method GET -Headers @{ Authorization="Bearer $Token" }
+
+    while ($result.state -ne $null) {
+        
+        Write-Verbose "Waiting for operation..."
+        Start-Sleep -Seconds 10
+        $result = Invoke-RestMethod  -Uri $uri -Method GET -Headers @{ Authorization="Bearer $Token" }
+    }
+}
+
+function Delete-ASAObject {
+    
+    param(
+   
+    [parameter(Mandatory=$true)]
+    [String]
+    $WorkspaceName,
+
+    [parameter(Mandatory=$true)]
+    [String]
+    $Category,
+
+    [parameter(Mandatory=$true)]
+    [String]
+    $Name,
+
+    [parameter(Mandatory=$true)]
+    [String]
+    $Token 
+    )
+
+    $uri = "https://$($WorkspaceName).dev.azuresynapse.net/$($Category)/$($Name)?api-version=2019-06-01-preview"
+
+    $result = Invoke-RestMethod  -Uri $uri -Method DELETE -Headers @{ Authorization="Bearer $Token" }
+    
+    return $result
+}
+
 Export-ModuleMember -Function List-StorageAccountKeys
 Export-ModuleMember -Function List-CosmosDBKeys
 Export-ModuleMember -Function Create-KeyVaultLinkedService
 Export-ModuleMember -Function Create-BlobStorageLinkedService
+Export-ModuleMember -Function Create-DataLakeLinkedService
 Export-ModuleMember -Function Create-CosmosDBLinkedService
+Export-ModuleMember -Function Create-SQLPoolKeyVaultLinkedService
 Export-ModuleMember -Function Create-IntegrationRuntime
+Export-ModuleMember -Function Create-Dataset
+Export-ModuleMember -Function Create-Pipeline
+Export-ModuleMember -Function Run-Pipeline
+Export-ModuleMember -Function Get-PipelineRun
+Export-ModuleMember -Function Get-OperationResult
+Export-ModuleMember -Function Wait-ForOperation
+Export-ModuleMember -Function Delete-ASAObject
