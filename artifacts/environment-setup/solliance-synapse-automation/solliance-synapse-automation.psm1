7e616b7af958cc596d4803aca7e7218224cd5d82
Ciprian Jichici <ciprian.jichici@genisoft.eu>
2020-04-28 09:13:56 +0300

    Setup environment

--- artifacts/environment-setup/solliance-synapse-automation/solliance-synapse-automation.psm1
+++ artifacts/environment-setup/solliance-synapse-automation/solliance-synapse-automation.psm1
@@ -20,13 +20,44 @@ function List-StorageAccountKeys {
 
     $uri = "https://management.azure.com/subscriptions/$($SubscriptionId)/resourcegroups/$($ResourceGroupName)/providers/Microsoft.Storage/storageAccounts/$($Name)/listKeys?api-version=2015-05-01-preview"
 
-    Write-Output "Calling endpoint $uri"
+    Write-Debug "Calling endpoint $uri"
 
     $result = Invoke-RestMethod  -Uri $uri -Method POST -Headers @{ Authorization="Bearer $Token" } -ContentType "application/json"
  
-    Write-Output $result
+    Write-Debug $result
 
     return $result.key1
+}
+
+function List-CosmosDBKeys {
+
+    param(
+    [parameter(Mandatory=$true)]
+    [String]
+    $SubscriptionId,
+
+    [parameter(Mandatory=$true)]
+    [String]
+    $ResourceGroupName,
+
+    [parameter(Mandatory=$true)]
+    [String]
+    $Name,
+
+    [parameter(Mandatory=$true)]
+    [String]
+    $Token
+    )
+
+    $uri = "https://management.azure.com/subscriptions/$($SubscriptionId)/resourcegroups/$($ResourceGroupName)/providers/Microsoft.DocumentDB/databaseAccounts/$($Name)/listKeys?api-version=2016-03-31"
+
+    Write-Debug "Calling endpoint $uri"
+
+    $result = Invoke-RestMethod  -Uri $uri -Method POST -Headers @{ Authorization="Bearer $Token" } -ContentType "application/json"
+ 
+    Write-Debug $result
+
+    return $result.primaryMasterKey
 }
 
 function Create-KeyVaultLinkedService {
@@ -50,14 +81,14 @@ function Create-KeyVaultLinkedService {
     )
 
     $keyVaultTemplate = Get-Content -Path "$($TemplatesPath)/key_vault_linked_service.json"
-    $keyVault = $keyVaultTemplate.Replace("#KEY_VAULT_NAME#", $Name)
+    $keyVault = $keyVaultTemplate.Replace("#LINKED_SERVICE_NAME#", $Name).Replace("#KEY_VAULT_NAME#", $Name)
     $uri = "https://$($WorkspaceName).dev.azuresynapse.net/linkedservices/$($Name)?api-version=2019-06-01-preview"
 
-    Write-Output "Calling endpoint $uri"
+    Write-Debug "Calling endpoint $uri"
 
     $result = Invoke-RestMethod  -Uri $uri -Method PUT -Body $keyVault -Headers @{ Authorization="Bearer $Token" } -ContentType "application/json"
  
-    Write-Output $result
+    Write-Debug $result
 }
 
 function Create-BlobStorageLinkedService {
@@ -77,18 +108,61 @@ function Create-BlobStorageLinkedService {
 
     [parameter(Mandatory=$true)]
     [String]
+    $Key,
+
+    [parameter(Mandatory=$true)]
+    [String]
     $Token 
     )
 
     $keyVaultTemplate = Get-Content -Path "$($TemplatesPath)/blob_storage_linked_service.json"
-    $keyVault = $keyVaultTemplate.Replace("#STORAGE_ACCOUNT_NAME#", $Name)
+    $keyVault = $keyVaultTemplate.Replace("#LINKED_SERVICE_NAME#", $Name).Replace("#STORAGE_ACCOUNT_NAME#", $Name).Replace("#STORAGE_ACCOUNT_KEY#", $Name)
     $uri = "https://$($WorkspaceName).dev.azuresynapse.net/linkedservices/$($Name)?api-version=2019-06-01-preview"
 
-    Write-Output "Calling endpoint $uri"
+    Write-Debug "Calling endpoint $uri"
 
     $result = Invoke-RestMethod  -Uri $uri -Method PUT -Body $keyVault -Headers @{ Authorization="Bearer $Token" } -ContentType "application/json"
  
-    Write-Output $result
+    Write-Debug $result
+}
+
+function Create-CosmosDBLinkedService {
+    
+    param(
+    [parameter(Mandatory=$true)]
+    [String]
+    $TemplatesPath,
+
+    [parameter(Mandatory=$true)]
+    [String]
+    $WorkspaceName,
+
+    [parameter(Mandatory=$true)]
+    [String]
+    $Name,
+
+    [parameter(Mandatory=$true)]
+    [String]
+    $Database,
+
+    [parameter(Mandatory=$true)]
+    [String]
+    $Key,
+
+    [parameter(Mandatory=$true)]
+    [String]
+    $Token 
+    )
+
+    $cosmosDbTemplate = Get-Content -Path "$($TemplatesPath)/cosmos_db_linked_service.json"
+    $cosmosDb = $cosmosDbTemplate.Replace("#LINKED_SERVICE_NAME#", $Name).Replace("#COSMOSDB_ACCOUNT_NAME#", $Name).Replace("#COSMOSDB_DDATABASE_NAME#", $Name).Replace("#COSMOSDB_ACCOUNT_KEY#", $Name)
+    $uri = "https://$($WorkspaceName).dev.azuresynapse.net/linkedservices/$($Name)?api-version=2019-06-01-preview"
+
+    Write-Debug "Calling endpoint $uri"
+
+    $result = Invoke-RestMethod  -Uri $uri -Method PUT -Body $keyVault -Headers @{ Authorization="Bearer $Token" } -ContentType "application/json"
+ 
+    Write-Debug $result
 }
 
 function Create-IntegrationRuntime {
@@ -139,6 +213,8 @@ function Create-IntegrationRuntime {
 }
 
 Export-ModuleMember -Function List-StorageAccountKeys
+Export-ModuleMember -Function List-CosmosDBKeys
 Export-ModuleMember -Function Create-KeyVaultLinkedService
 Export-ModuleMember -Function Create-BlobStorageLinkedService
+Export-ModuleMember -Function Create-CosmosDBLinkedService
 Export-ModuleMember -Function Create-IntegrationRuntime
