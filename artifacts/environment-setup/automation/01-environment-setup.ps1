c9bfda16441fcf5ae7963539edc2725e52f90cdd
Ciprian Jichici <ciprian.jichici@genisoft.eu>
2020-05-02 05:05:04 +0300

    Setup environment

--- artifacts/environment-setup/automation/01-environment-setup.ps1
+++ artifacts/environment-setup/automation/01-environment-setup.ps1
@@ -301,9 +301,9 @@ $sqlScripts = [ordered]@{
 
 foreach ($sqlScriptName in $sqlScripts.Keys) {
         $sqlScriptFileName = "$($sqlScripts[$sqlScriptName])\$($sqlScriptName).sql"
-        Write-Information "Creating SQL script $($sqlScriptName) from $($sqlScriptFileName)"
-        $result = Create-SQLScript -TemplatesPath $templatesPath -WorkspaceName $workspaceName -Name $sqlScriptName -TemplateFileName "sql_script" -ScriptFileName $sqlScriptFileName -Token $synapseToken
-        Wait-ForOperation -WorkspaceName $workspaceName -OperationId $result.operationId -Token $synapseToken
+        #Write-Information "Creating SQL script $($sqlScriptName) from $($sqlScriptFileName)"
+        #$result = Create-SQLScript -TemplatesPath $templatesPath -WorkspaceName $workspaceName -Name $sqlScriptName -TemplateFileName "sql_script" -ScriptFileName $sqlScriptFileName -Token $synapseToken
+        #Wait-ForOperation -WorkspaceName $workspaceName -OperationId $result.operationId -Token $synapseToken
 }
 
 
