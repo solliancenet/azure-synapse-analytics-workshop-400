6865ea5f7469776e43de29de06313f85fdb60e0b
Ciprian Jichici <ciprian.jichici@genisoft.eu>
2020-05-12 09:02:36 +0300

    Environment cleanup

---
+++ artifacts/environment-setup/automation/08-05-environment-cleanup-sql-rebuild-sale-small.ps1
@@ -0,0 +1,125 @@
+Remove-Module solliance-synapse-automation
+Import-Module ".\artifacts\environment-setup\solliance-synapse-automation"
+
+$InformationPreference = "Continue"
+
+# These need to be run only if the Az modules are not yet installed
+# Install-Module -Name Az -AllowClobber -Scope CurrentUser
+# Install-Module -Name Az.CosmosDB -AllowClobber -Scope CurrentUser
+# Import-Module Az.CosmosDB
+
+#
+# TODO: Keep all required configuration in C:\LabFiles\AzureCreds.ps1 file
+. C:\LabFiles\AzureCreds.ps1
+
+$userName = $AzureUserName                # READ FROM FILE
+$password = $AzurePassword                # READ FROM FILE
+$clientId = $TokenGeneratorClientId       # READ FROM FILE
+$sqlPassword = $AzureSQLPassword          # READ FROM FILE
+
+$securePassword = $password | ConvertTo-SecureString -AsPlainText -Force
+$cred = new-object -typename System.Management.Automation.PSCredential -argumentlist $userName, $SecurePassword
+
+Connect-AzAccount -Credential $cred | Out-Null
+
+$resourceGroupName = (Get-AzResourceGroup | Where-Object { $_.ResourceGroupName -like "*L400*" }).ResourceGroupName
+$uniqueId =  (Get-AzResourceGroup -Name $resourceGroupName).Tags["DeploymentId"]
+$subscriptionId = (Get-AzContext).Subscription.Id
+$tenantId = (Get-AzContext).Tenant.Id
+
+$templatesPath = ".\artifacts\environment-setup\templates"
+$datasetsPath = ".\artifacts\environment-setup\datasets"
+$pipelinesPath = ".\artifacts\environment-setup\pipelines"
+$sqlScriptsPath = ".\artifacts\environment-setup\sql"
+$workspaceName = "asaworkspace$($uniqueId)"
+$cosmosDbAccountName = "asacosmosdb$($uniqueId)"
+$cosmosDbDatabase = "CustomerProfile"
+$cosmosDbContainer = "OnlineUserProfile01"
+$dataLakeAccountName = "asadatalake$($uniqueId)"
+$blobStorageAccountName = "asastore$($uniqueId)"
+$keyVaultName = "asakeyvault$($uniqueId)"
+$keyVaultSQLUserSecretName = "SQL-USER-ASA"
+$sqlPoolName = "SQLPool01"
+$integrationRuntimeName = "AzureIntegrationRuntime01"
+$sparkPoolName = "SparkPool01"
+$amlWorkspaceName = "amlworkspace$($uniqueId)"
+
+
+$ropcBodyCore = "client_id=$($clientId)&username=$($userName)&password=$($password)&grant_type=password"
+$global:ropcBodySynapse = "$($ropcBodyCore)&scope=https://dev.azuresynapse.net/.default"
+$global:ropcBodyManagement = "$($ropcBodyCore)&scope=https://management.azure.com/.default"
+$global:ropcBodySynapseSQL = "$($ropcBodyCore)&scope=https://sql.azuresynapse.net/.default"
+
+$global:synapseToken = ""
+$global:synapseSQLToken = ""
+$global:managementToken = ""
+
+$global:tokenTimes = [ordered]@{
+        Synapse = (Get-Date -Year 1)
+        SynapseSQL = (Get-Date -Year 1)
+        Management = (Get-Date -Year 1)
+}
+
+
+Write-Information "Start the $($sqlPoolName) SQL pool if needed."
+
+$result = Get-SQLPool -SubscriptionId $subscriptionId -ResourceGroupName $resourceGroupName -WorkspaceName $workspaceName -SQLPoolName $sqlPoolName
+if ($result.properties.status -ne "Online") {
+    Control-SQLPool -SubscriptionId $subscriptionId -ResourceGroupName $resourceGroupName -WorkspaceName $workspaceName -SQLPoolName $sqlPoolName -Action resume
+    Wait-ForSQLPool -SubscriptionId $subscriptionId -ResourceGroupName $resourceGroupName -WorkspaceName $workspaceName -SQLPoolName $sqlPoolName -TargetStatus Online
+}
+
+
+Write-Information "Create tables in the [wwi] schema in $($sqlPoolName)"
+
+$params = @{}
+$result = Execute-SQLScriptFile -SQLScriptsPath $sqlScriptsPath -WorkspaceName $workspaceName -SQLPoolName $sqlPoolName -FileName "04-create-tables-in-wwi-schema" -Parameters $params
+$result
+
+
+Write-Information "Create data sets for data load in SQL pool $($sqlPoolName)"
+
+$loadingDatasets = @{
+        wwi02_date_adls = $dataLakeAccountName
+        wwi02_product_adls = $dataLakeAccountName
+        wwi02_sale_small_adls = $dataLakeAccountName
+        wwi02_date_asa = $sqlPoolName.ToLower()
+        wwi02_product_asa = $sqlPoolName.ToLower()
+        wwi02_sale_small_asa = "$($sqlPoolName.ToLower())_highperf"
+}
+
+foreach ($dataset in $loadingDatasets.Keys) {
+        Write-Information "Creating dataset $($dataset)"
+        $result = Create-Dataset -DatasetsPath $datasetsPath -WorkspaceName $workspaceName -Name $dataset -LinkedServiceName $loadingDatasets[$dataset]
+        Wait-ForOperation -WorkspaceName $workspaceName -OperationId $result.operationId
+}
+
+Write-Information "Create pipeline to load the SQL pool"
+
+$params = @{
+        BLOB_STORAGE_LINKED_SERVICE_NAME = $blobStorageAccountName
+}
+$loadingPipelineName = "Setup - Load SQL Pool"
+$fileName = "load_sql_pool_from_data_lake"
+
+Write-Information "Creating pipeline $($loadingPipelineName)"
+
+$result = Create-Pipeline -PipelinesPath $pipelinesPath -WorkspaceName $workspaceName -Name $loadingPipelineName -FileName $fileName -Parameters $params
+Wait-ForOperation -WorkspaceName $workspaceName -OperationId $result.operationId
+
+Write-Information "Running pipeline $($loadingPipelineName)"
+
+$result = Run-Pipeline -WorkspaceName $workspaceName -Name $loadingPipelineName
+$result = Wait-ForPipelineRun -WorkspaceName $workspaceName -RunId $result.runId
+$result
+
+Write-Information "Deleting pipeline $($loadingPipelineName)"
+
+$result = Delete-ASAObject -WorkspaceName $workspaceName -Category "pipelines" -Name $loadingPipelineName
+Wait-ForOperation -WorkspaceName $workspaceName -OperationId $result.operationId
+
+foreach ($dataset in $loadingDatasets.Keys) {
+        Write-Information "Deleting dataset $($dataset)"
+        $result = Delete-ASAObject -WorkspaceName $workspaceName -Category "datasets" -Name $dataset
+        Wait-ForOperation -WorkspaceName $workspaceName -OperationId $result.operationId
+}
