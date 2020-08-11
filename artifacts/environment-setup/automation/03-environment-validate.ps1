3a9a3c37d87b874742ef5e56ec6100595342eefb
srushti-714 <55180169+srushti-714@users.noreply.github.com>
2020-08-11 20:05:30 +0530

    Update 03-environment-validate.ps1
    Removed validation code for checking user and table wwi_ml.MLModelExt as we are ignoring issues of setup script

--- artifacts/environment-setup/automation/03-environment-validate.ps1
+++ artifacts/environment-setup/automation/03-environment-validate.ps1
@@ -265,12 +265,6 @@ if ($sqlPool -eq $null) {
                         Valid = $false
                         ValidCount = $false
                 }
-                "wwi_ml.MLModelExt" = @{
-                        Count = 1
-                        StrictCount = $true
-                        Valid = $false
-                        ValidCount = $false
-                }
                 "wwi_ml.MLModel" = @{
                         Count = 0
                         StrictCount = $true
@@ -356,28 +350,6 @@ FROM
                 "$($userName)" = @{ Valid = $false }
         }
 
-$query = @"
-select name from sys.sysusers
-"@
-        #$result = Execute-SQLQuery -WorkspaceName $workspaceName -SQLPoolName $sqlPoolName -SQLQuery $query
-        $result = Invoke-SqlCmd -Query $query -ServerInstance $sqlEndpoint -Database $sqlPoolName -Username $sqlUser -Password $sqlPassword
-
-        #foreach ($dataRow in $result.data) {
-        foreach ($dataRow in $result) {
-                $name = $dataRow[0]
-
-                if ($users[$name]) {
-                        Write-Information "Found user $($name)."
-                        $users[$name]["Valid"] = $true
-                }
-        }
-
-        foreach ($name in $users.Keys) {
-                if (-not $users[$name]["Valid"]) {
-                        Write-Warning "User $($name) was not found."
-                        $overallStateIsValid = $false
-                }
-        }
 }
 
 Write-Information "Checking Spark pool $($sparkPoolName)"
